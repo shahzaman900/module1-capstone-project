9f6f00841a8eae1ffae6dd5eb650c7e18bffeb98
sheezu900 <shahzamanlive2@gmail.com>
2023-06-29 16:58:55 +0500

    complete all

--- js/dist/mainSpeakers.dev.js
+++ js/dist/mainSpeakers.dev.js
@@ -2,29 +2,24 @@
 
 var speakers = [{
   speakerImage: './images/speaker_01.png',
-  speakerName: 'Shah Zaman',
-  profession: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-  discription: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
+  speakerName: 'David Fitch',
+  profession: 'Storyteller Panel:',
+  discription: 'Karen Wilk , David Lundberg, Taehoo Lee6'
 }, {
-  speakerImage: './images/speaker_01.png',
-  speakerName: 'Shah Zaman',
-  profession: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-  discription: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
+  speakerImage: './images/speaker_02.png',
+  speakerName: 'Rachel Beveridge',
+  profession: 'Storyteller Panel:',
+  discription: 'Sebastian Maldonado, Kurt Rietema, Sparrow Etter Carlson'
 }, {
-  speakerImage: './images/speaker_01.png',
-  speakerName: 'Shah Zaman',
-  profession: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-  discription: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
+  speakerImage: './images/speaker_03.png',
+  speakerName: 'John Eigege',
+  profession: 'Storyteller Panel:',
+  discription: 'Szabina Sztojka, Andrew Beunk, Laura Osborne'
 }, {
-  speakerImage: './images/speaker_01.png',
-  speakerName: 'Shah Zaman',
-  profession: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-  discription: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
-}, {
-  speakerImage: './images/speaker_01.png',
-  speakerName: 'Shah Zaman',
-  profession: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-  discription: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
+  speakerImage: './images/speaker_04.png',
+  speakerName: 'Ruth Padilla-DeBorst',
+  profession: 'Storyteller Panel:',
+  discription: 'Mario Matos, John Lee, David Kromminga'
 }];
 var speakerTwoCard = [{
   speakerImage: './images/speaker_01.png',
@@ -52,7 +47,7 @@ function speakersTwoCards() {
   speakerTwoCard.map(function (card) {
     var speakerCard = document.createElement('div');
     speakerCard.classList.add('card');
-    speakerCard.innerHTML = "\n          <div class=\"card\">\n            <img src=\"".concat(card.speakerImage, "\" alt=\"\">\n            <div class=\"content\">\n              <h2>").concat(card.speakerName, "</h2>\n              <h4>").concat(card.profession, "</h4>\n              <hr>\n              <p>").concat(card.discription, "</p>\n            </div>\n          </div>");
+    speakerCard.innerHTML = "\n          <div class=\"card\">\n            <img src=\"".concat(card.speakerImage, "\" alt=\"\">\n            <div class=\"content\">\n              <h2>").concat(card.speakerName, "</h2>\n              <hr>\n              <h4>").concat(card.profession, "</h4>\n              <p>").concat(card.discription, "</p>\n            </div>\n          </div>");
     return mainSpeaker.appendChild(speakerCard);
   });
 }
--- js/mainSpeakers.js
+++ js/mainSpeakers.js
@@ -2,33 +2,27 @@
 let speakers = [
   {
     speakerImage: './images/speaker_01.png',
-    speakerName: 'Shah Zaman',
-    profession: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    discription: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
+    speakerName: 'David Fitch',
+    profession: 'Storyteller Panel:',
+    discription: 'Karen Wilk , David Lundberg, Taehoo Lee6'
   },
   {
-    speakerImage: './images/speaker_01.png',
-    speakerName: 'Shah Zaman',
-    profession: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    discription: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
+    speakerImage: './images/speaker_02.png',
+    speakerName: 'Rachel Beveridge',
+    profession: 'Storyteller Panel:',
+    discription: 'Sebastian Maldonado, Kurt Rietema, Sparrow Etter Carlson'
   },
   {
-    speakerImage: './images/speaker_01.png',
-    speakerName: 'Shah Zaman',
-    profession: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    discription: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
+    speakerImage: './images/speaker_03.png',
+    speakerName: 'John Eigege',
+    profession: 'Storyteller Panel:',
+    discription: 'Szabina Sztojka, Andrew Beunk, Laura Osborne'
   },
   {
-    speakerImage: './images/speaker_01.png',
-    speakerName: 'Shah Zaman',
-    profession: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    discription: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
-  },
-  {
-    speakerImage: './images/speaker_01.png',
-    speakerName: 'Shah Zaman',
-    profession: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    discription: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
+    speakerImage: './images/speaker_04.png',
+    speakerName: 'Ruth Padilla-DeBorst',
+    profession: 'Storyteller Panel:',
+    discription: 'Mario Matos, John Lee, David Kromminga'
   },
 ];
 
@@ -80,8 +74,8 @@ function speakersTwoCards() {
             <img src="${card.speakerImage}" alt="">
             <div class="content">
               <h2>${card.speakerName}</h2>
-              <h4>${card.profession}</h4>
               <hr>
+              <h4>${card.profession}</h4>
               <p>${card.discription}</p>
             </div>
           </div>`
